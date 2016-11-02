5c8b7f958c1bc5d700588824115309fe7db3ee1a
strawlink <theodorstraarup@gmail.com>
2016-11-02 21:16:22 +0100

    Initial commit.

---
+++ UnityProject/Assets/Scripts/InputController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+	public static InputController Instance { get; private set; }
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+}
